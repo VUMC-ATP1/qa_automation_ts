067b1b4cd5c682e376f4b66a6fbedacfa5a7092e
taisijasmirnova <tasjuha94@gmail.com>
2022-07-05 19:12:50 +0300

    Classroom

---
+++ src/main/java/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // add fields
+    // name, surname, year, role
+    // all args constructor + no args constructor
+    // getter/setter
+    // test in under test/java package
+    // goal: 100% coverage
+}
